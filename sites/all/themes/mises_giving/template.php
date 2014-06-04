33dbb9b2ebc5cef555412f9c5ba5e8cced1a9ce4
PridyOK <aaron.pridy@gmail.com>
2014-06-04 23:33:04 +0000

    Template file creation and Giving theme development

--- sites/all/themes/mises_giving/template.php
+++ sites/all/themes/mises_giving/template.php
@@ -53,6 +53,7 @@ function mises_giving_process_html(&$vars) {
 /**
  * Override or insert variables for the page templates.
  */
+
 /* -- Delete this line if you want to use these functions
 function mises_giving_preprocess_page(&$vars) {
 }
