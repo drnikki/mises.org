a0311ce13c48719aa42bae631e34cb9c5c879608
Anne Easterling <anne@newvalleymedia.com>
2014-07-01 01:35:19 +0000

    1897842 profile: adjustments to profile sub-menu view to use proper tax; clean up debugging code; minor styling additions; add simple teaser to legacy_book and book ct; add mises admin page to feature

--- sites/all/themes/bootsass/theme/views/views-view-summary-unformatted--author_articles--block-2.tpl.php
+++ sites/all/themes/bootsass/theme/views/views-view-summary-unformatted--author_articles--block-2.tpl.php
@@ -9,7 +9,7 @@
       //print '<pre>';
       //print_r($row);
       //print '</pre>';
-    $term_name = $row->taxonomy_term_data_node_name;
+    $term_name = $row->taxonomy_term_data_field_data_field_library_item_tree_name;
     $term = taxonomy_get_term_by_name($term_name);
     $ms_term = array_shift($term);
     $ms_search_url = '/search/site/?f[0]=sm_field_author_1%3Anode%3A'.arg(1).'&f[1]=im_field_library_item_tree%3A'.$ms_term->tid;
