652b1375f3c6e12c0e8a9bdb586bdef08643189e
Anne Easterling <anne@newvalleymedia.com>
2014-09-23 20:41:11 +0000

    2404918 search blocks: add alter for apache page form; update styles to standardize all search forms

--- sites/all/themes/bootsass/theme/bootsass/bootsass-search-form-wrapper.func.php
+++ sites/all/themes/bootsass/theme/bootsass/bootsass-search-form-wrapper.func.php
@@ -11,14 +11,8 @@ function bootsass_bootsass_search_form_wrapper($variables) {
   $output = '<div class="input-group">';
   $output .= $variables['element']['#children'];
   $output .= '<span class="input-group-btn">';
-  $output .= '<button type="submit" class="btn btn-white">';
-  // We can be sure that the font icons exist in theme.
-  if (file_exists(drupal_get_path('theme', 'bootsass') . '/fonts/bootstrap/glyphicons-halflings-regular.ttf')) {
-    $output .= _bootsass_icon('search');
-  }
-  else {
-    $output .= t('Search');
-  }
+  $output .= '<button type="submit" class="btn btn-default">';
+  $output .= t('GO');
   $output .= '</button>';
   $output .= '</span>';
   $output .= '</div>';
