5b3ca1ad2861cfda73319bd08695278eed0f083c
Josh Estep <josh.estep@newvalleymedia.com>
2014-10-06 14:18:09 +0000

    Added jQuery to hide content type, audience, and media types: quote and online texts within top facets

--- sites/all/themes/bootsass/js/solr_top_facets.js
+++ sites/all/themes/bootsass/js/solr_top_facets.js
@@ -14,16 +14,27 @@ var Drupal = Drupal || {};
     attach: function(context) {
        $('body.page-search #search-form #edit-basic').before('<div id="solr-facet-tags"></div>');
        var facetCount = 0;
+       var facetName = '';
        $('body.page-search .block-facetapi a').each(function () {
+         var ggParent = $(this).parent().parent().parent();
+         ggParent.children('h2').each(function () {
+           facetName = $(this).text();
+         });
+
          if ($(this).hasClass('facetapi-active')) {
            var facetParent = $(this).parent();
            facetParent.contents().each(function () {
              if ($(this).prop('tagName') === undefined) {
-               $('#solr-facet-tags').append('<div id="facet-tag-' + facetCount + '" class="facet-tag btn btn-white btn-sm"> <span class="glyphicon glyphicon-remove"></span>' + $(this).text() + '</div>');
-               $('#facet-tag-' + facetCount).click(function () {
-                 facetParent.children('input.facetapi-checkbox').click();
-               });
-               facetCount++;
+               if (facetName != 'Content type' && facetName != 'Audience' &&
+                 !(facetName == 'Media type' && ($(this).text() == 'Quotes' || $(this).text() == 'Online Texts'))
+               ) {
+                 $('#solr-facet-tags').append('<div id="facet-tag-' + facetCount +
+                   '" class="facet-tag btn btn-white btn-sm"> <span class="glyphicon glyphicon-remove"></span>' + $(this).text() + '</div>');
+                 $('#facet-tag-' + facetCount).click(function () {
+                   facetParent.children('input.facetapi-checkbox').click();
+                 });
+                 facetCount++;
+               }
              }
            });
          }
