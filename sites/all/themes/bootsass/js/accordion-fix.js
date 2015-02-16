2d85aa97b6c6e826d75379fff8a29c24b1806b6b
Josh Estep <jestep@118105.no-reply.drupal.org>
2015-02-16 11:50:41 +0000

    Applying accordion changes for sub-page support.

--- sites/all/themes/bootsass/js/accordion-fix.js
+++ sites/all/themes/bootsass/js/accordion-fix.js
@@ -12,13 +12,58 @@ var Drupal = Drupal || {};
       $('.view-book-content .accordion-toggle').each(function () {
         $(this).addClass('collapsed');
       });
-      $('.view-book-content .accordion-toggle').click(function () {
+
+      $('.view-book-content h3.accordion-toggle').click(function () {
         if ($(this).hasClass('collapsed') == false) {
           $(this).parent().parent().find('h3').each(function () {
             $(this).addClass('collapsed');
           });
+          $(this).parent().parent().find('.chapter-intro').each(function () {
+            $(this).slideDown();
+          });
+          $(this).parent().find('.subpage-content').each(function () {
+            $(this).slideDown();
+            $(this).parent().find('h4').each(function () {
+              $(this).removeClass('collapsed');
+            });
+          });
+        } else {
+          $(this).parent().parent().find('h3').each(function () {
+            $(this).removeClass('collapsed');
+          });
+          $(this).parent().parent().find('.chapter-intro').each(function () {
+            $(this).slideUp();
+          });
+          $(this).parent().find('.subpage-content').each(function () {
+            $(this).slideUp();
+            $(this).parent().find('h4').each(function () {
+              $(this).addClass('collapsed');
+            });
+          });
         }
       });
+
+      $('.view-book-content h4.accordion-toggle').click(function () {
+        if ($(this).hasClass('collapsed') == false) {
+          $(this).addClass('collapsed');
+          $(this).parent().find('.subpage-content').each(function () {
+            $(this).parent().parent().find('.page-content').each(function () {
+              $(this).slideDown();
+            });
+            $(this).slideUp();
+          });
+        } else {
+          $(this).removeClass('collapsed');
+          $(this).parent().find('.subpage-content').each(function () {
+            $(this).parent().parent().find('.page-content').each(function () {
+              $(this).slideUp();
+            });
+            $(this).slideDown();
+          });
+        }
+      });
+ 
+
       var tmpFrag = window.location.hash.replace('#', '');
       tmpFrag = decodeURIComponent((tmpFrag + '').replace(/\+/g, '%20'));
       $('h2.teaser-title').each(function () {
