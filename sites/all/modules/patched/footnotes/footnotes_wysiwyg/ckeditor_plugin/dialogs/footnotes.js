e1c268e9621791be22e7b700fea31dad1ee7fba7
Anne Easterling <anne@newvalleymedia.com>
2015-06-24 00:51:02 +0000

    3432666 long footnotes: update ckeditor to v4.4.7; patch ckeditor module to allow cke4; patch footnotes js to use text area; merge patches togther; add ckeditor.dtd declarations

---
+++ sites/all/modules/patched/footnotes/footnotes_wysiwyg/ckeditor_plugin/dialogs/footnotes.js
@@ -0,0 +1,92 @@
+(function() {
+  function footnotesDialog( editor, isEdit ) {
+    return {
+      title : Drupal.t('Footnotes Dialog'),
+      minWidth : 500,
+      minHeight : 50,
+      contents : [
+        {
+          id: 'info',
+          label: Drupal.t('Add a footnote'),
+          title: Drupal.t('Add a footnote'),
+          elements:
+          [
+            {
+              id: 'footnote',
+              type: 'textarea',
+              label: Drupal.t('Footnote text :'),
+              onLoad : function () {
+                // Workaround.  CKEditor 3.x does not add the ID to the input,
+                // just a wrapper <div> several levels above.  Add a class explicitly here
+                // so we can find the textarea easily later.
+                var footnote_id = this.getElement().getId();
+                var footnoteWrapper = jQuery("#" + footnote_id);
+                var textarea = jQuery(".cke_dialog_ui_input_textarea textarea", footnoteWrapper);
+                textarea.addClass('footnote-textarea');
+                console.log('footnot_id ', footnote_id);
+              },
+              setup: function (element) {
+                if (isEdit)
+                  this.setValue(element.getHtml());
+              }
+            },
+            {
+              id: 'value',
+              type: 'text',
+              label: Drupal.t('Value :'),
+              labelLayout: 'horizontal',
+              style: 'float:left;width:100px;',
+              setup: function (element) {
+                if (isEdit)
+                  this.setValue(element.getAttribute('value'));
+              }
+            }
+          ],
+        }
+      ],
+      onShow : function() {
+        if (isEdit) {
+          this.fakeObj = CKEDITOR.plugins.footnotes.getSelectedFootnote( editor );
+          this.realObj = editor.restoreRealElement( this.fakeObj );
+        }
+        this.setupContent( this.realObj );
+        var current_editor_id = this.getParentEditor().id;
+        var footnote_editor_id = jQuery('.footnote-textarea').attr('id');
+  
+        // Replace the Footnote textarea with a CKEditor instance.
+        CKEDITOR.replace(footnote_editor_id, {
+          toolbar: [
+            { name: 'basicstyles', items : [ 'Bold','Italic', 'Underline', 'Link', 'Unlink','BulletedList','NumberedList','Blockquote' ] }
+          ],
+          enterMode: CKEDITOR.ENTER_BR,
+          autoParagraph : true,
+          resize_enabled : false,
+          autoGrow_minHeight : 80,
+          removePlugins : 'footnotes',
+          startupFocus: true
+        });
+       },
+      onOk : function() {
+        var footnote_editor_id = jQuery('.footnote-textarea').attr('id');
+        var content = CKEDITOR.instances[footnote_editor_id].getData();
+        CKEDITOR.plugins.footnotes.createFootnote( editor, this.realObj, content, this.getValueOf('info', 'value'));
+        delete this.fakeObj;
+        delete this.realObj;
+
+        CKEDITOR.instances[footnote_editor_id].destroy();
+
+      },
+      onCancel : function() {
+        var footnote_editor_id = jQuery('.footnote-textarea').attr('id');
+        CKEDITOR.instances[footnote_editor_id].destroy();
+      }
+    };
+  }
+
+  CKEDITOR.dialog.add( 'createfootnotes', function( editor ) {
+    return footnotesDialog( editor );
+  });
+  CKEDITOR.dialog.add( 'editfootnotes', function( editor ) {
+    return footnotesDialog( editor, 1 );
+  });
+})();
