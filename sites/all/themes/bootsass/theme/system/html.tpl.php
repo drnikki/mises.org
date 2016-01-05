c48c74aeec20680a55f7528848af9b156c8d95f2
Will Jackson <will.jackson@newvalleymedia.com>
2016-01-05 20:23:50 +0000

    Add Alexa certification script.

--- sites/all/themes/bootsass/theme/system/html.tpl.php
+++ sites/all/themes/bootsass/theme/system/html.tpl.php
@@ -47,6 +47,15 @@
   "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces;?>>
 <head profile="<?php print $grddl_profile; ?>">
+  
+  <!-- Start Alexa Certify Javascript -->
+  <script type="text/javascript">
+  _atrk_opts = { atrk_acct:"1sqqm1akKd60uG", domain:"mises.org",dynamic: true};
+  (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+  </script>
+  <noscript><img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=1sqqm1akKd60uG" style="display:none" height="1" width="1" alt="" /></noscript>
+  <!-- End Alexa Certify Javascript -->  
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="twitter:widgets:csp" content="on">
