1da545d3b80754b97905c58dce3c414ffb8e373a
Carlos Zuñiga <czuniga.co@gmail.com>
2016-09-26 15:38:58 -0500

    Move hero block on mobile

---
+++ sites/all/themes/bootsass/js/homepage_hero.js
@@ -0,0 +1,37 @@
+(function($, Drupal){
+  /**
+   * Move hero to main column on mobile
+   */
+  Drupal.behaviors.homepageHero = {
+    attach: function(context, settings) {
+
+      moveHeroBlock();
+      $(window).resize(function () {
+        moveHeroBlock();
+      });
+
+      function moveHeroBlock() {
+        var heroBlock = $('#block-views-homepage-hero-block');
+        var parentElement = heroBlock.parent();
+        var screen_sm_min = 768;
+
+        if (screen.width >= screen_sm_min) {
+          if (!parentElement.hasClass('region-sidebar-second-top')) {
+            heroBlock.css('margin-bottom', '1em');
+            heroBlock.prependTo('.region-sidebar-second-top');
+            blockLocation = 'side';
+          }
+        }
+        else {
+          if (parentElement.hasClass('region-sidebar-second-top')) {
+            heroBlock.css('margin-bottom', '1em');
+            heroBlock.prependTo('.main-container .container .row > section');
+            blockLocation = 'main';
+          }
+        }
+
+      }
+    }
+  };
+
+})(jQuery, Drupal);
