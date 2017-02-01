227ecaad58348b1d3351be5528aa499ffea8a74e
Will Jackson <will@kanopistudios.com>
2017-02-01 18:40:11 -0500

    #11093882: Additional cleanup to template file.

--- sites/all/themes/bootsass/templates/block--google-cse.tpl.php
+++ sites/all/themes/bootsass/templates/block--google-cse.tpl.php
@@ -44,8 +44,8 @@
  * @ingroup themeable
  */
 ?>
-<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>
-  <div class="content"<?php print $content_attributes; ?>
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <div class="content"<?php print $content_attributes; ?>>
     <?php
     $content = str_replace("Enter your keywords", "", $content);
     $content = str_replace("Search", "GO", $content);
